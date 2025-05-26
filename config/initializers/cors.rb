2ce0dea8ace32930ddfd39c801c38d75d11e6b11
Patrick Bell <patrick@Patricks-Laptop.local>
2025-05-26 15:10:12 +0100

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
    end
 
    allow do
-    origins "https://minifigsmania.netlify.app"
+    origins 'https://683475bff4b6fd008427b749--minifigsmania.netlify.app'  # <-- your Netlify frontend URL here
 
     resource "*",
       headers: :any,
