d56fca59070a143bc3b2654466f899ccfea0cf11
Patrick Bell <patrick@Patricks-Laptop.local>
2025-05-26 18:28:45 +0100

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,49 +1,59 @@
 class SessionsController < ApplicationController
+  include ActionController::Cookies
+  JWT_SECRET_KEY = ENV['JWT_SECRET_KEY']
 
+  def create
+    user = User.find_by(email: params[:user][:email])
 
+    if user.nil?
+      render json: { error: 'Incorrect Email' }, status: :unauthorized
+      return
+    end
 
-    include ActionController::Cookies
-    JWT_SECRET_KEY = ENV['JWT_SECRET_KEY']
+    if user&.authenticate(params[:user][:password])
+      token = JWT.encode({ user_id: user.id, email: user.email, exp: 2.hours.from_now.to_i }, JWT_SECRET_KEY, 'HS256')
 
+      Rails.logger.debug "Generated Token: #{token}"
 
-    def create
-        user = User.find_by(email: params[:user][:email])
+      cookie_options = {
+        value: token,
+        httponly: true,
+        secure: Rails.env.production?,
+        same_site: :none,
+        expires: 2.hours.from_now,
+        path: "/"
+      }
+      cookie_options[:domain] = ".minifigsmania.netlify.app" if Rails.env.production?
 
-        if user.nil?
-          render json: { error: 'Incorrect Email' }, status: :unauthorized
-          return
-        end
-    
-        if user&.authenticate(params[:user][:password]) # NOT authenticate?
-          token = JWT.encode({ user_id: user.id, email: user.email, exp: 1.hour.from_now.to_i }, JWT_SECRET_KEY, 'HS256')
-    
-          Rails.logger.debug "Generated Token: #{token}"
-    
-          cookies[:token] = {
-            value: token,
-            httponly: true,
-            secure: Rails.env.production?,
-            same_site: :none,  # Adjust if needed
-            expires: 2.hours.from_now,
-          }
-    
-          render json: { message: 'Login successful', user: user, exp: 2.hours.from_now, token: token }
-        else
-          render json: { error: 'Invalid Password' }, status: :unauthorized
-        end
-      end
+      cookies[:token] = cookie_options
 
-      def destroy
-        cookies.delete(:token)
-        render json: { message: 'Logout successful' }
-      end
+      render json: {
+        message: 'Login successful',
+        user: user,
+        exp: 2.hours.from_now,
+        token: token
+      }
+    else
+      render json: { error: 'Invalid Password' }, status: :unauthorized
+    end
+  end
 
+  def destroy
+    cookie_options = {
+      path: "/"
+    }
+    cookie_options[:domain] = ".minifigsmania.netlify.app" if Rails.env.production?
 
-      def generate_jwt_token(user)
-        JWT.encode({ user_id: user.id, email: user.email, exp: 2.hours.from_now.to_i }, JWT_SECRET_KEY, 'HS256')
-      end
+    cookies.delete(:token, cookie_options)
 
+    render json: { message: 'Logout successful' }
+  end
 
-
-     
+  def generate_jwt_token(user)
+    JWT.encode(
+      { user_id: user.id, email: user.email, exp: 2.hours.from_now.to_i },
+      JWT_SECRET_KEY,
+      'HS256'
+    )
+  end
 end
